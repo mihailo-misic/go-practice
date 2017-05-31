02f88b868d8c829b8ee123e6ec5253434d7429bc
mihailomisic <mihailo.misic@gmail.com>
2017-05-31 17:45:40 +0200

    DB Retrieving data from DB

--- 05-db/db.go
+++ 05-db/db.go
@@ -28,6 +28,19 @@ func main() {
 func index(w http.ResponseWriter, r *http.Request) {
     _, err = io.WriteString(w, "Successfully completed.")
     check(err)
+
+    rows, err := db.Query(`SELECT firstname FROM users`)
+    check(err)
+
+    var s, name string
+
+    for rows.Next() {
+        s += "\n"
+        err = rows.Scan(&name)
+        check(err)
+        s += name
+    }
+    io.WriteString(w, s)
 }
 
 func check(err error) {
