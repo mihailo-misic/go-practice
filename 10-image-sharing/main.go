f3c450860afe174f559d5f3b5498727b2a38c604
mihailomisic <mihailo.misic@gmail.com>
2017-06-08 19:10:50 +0200

    BASICS DONE

--- 10-image-sharing/main.go
+++ 10-image-sharing/main.go
@@ -5,35 +5,51 @@ import (
     "log"
     "github.com/julienschmidt/httprouter"
     "html/template"
-    "github.com/astaxie/beego/orm"
     _ "github.com/go-sql-driver/mysql"
     "time"
     "net/url"
     "strings"
+    "golang.org/x/crypto/bcrypt"
+    "errors"
+    "github.com/jinzhu/gorm"
+    "fmt"
+    "crypto/sha1"
+    "io"
+    "os"
+    "path/filepath"
+    "strconv"
+    "io/ioutil"
 )
 
 var tpl *template.Template
 
 type User struct {
-    Id        int `orm:"auto"`
-    Email     string `orm:"size(100)"`
-    Password  string `orm:"size(100)"`
-    FirstName string `orm:"size(100)"`
-    LastName  string `orm:"size(100)"`
+    ID        uint
+    Email     string `gorm:"size:100"`
+    Password  string `gorm:"size:255"`
+    FirstName string `gorm:"size:100"`
+    LastName  string `gorm:"size:100"`
 }
 
-func (u *User) TableName() string {
-    return "users"
+type Data struct {
+    User   User
+    Images []string
 }
+
+var db *gorm.DB
 
 func init() {
     tpl = template.Must(template.ParseGlob("./templates/*"))
-    
-    orm.RegisterModel(new(User))
-    orm.RegisterDataBase("default", "mysql", "root:secret@/practice?charset=utf8", 30)
 }
 
 func main() {
+    var err error
+    db, err = gorm.Open("mysql", "root:secret@/practice?charset=utf8&parseTime=True")
+    if err != nil {
+        log.Fatal("Could not connect to database")
+    }
+    defer db.Close()
+
     router := httprouter.New()
     router.GET("/", Index)
     router.GET("/login", Login)
@@ -42,21 +58,45 @@ func main() {
     router.POST("/register-submit", ProcessRegister)
     router.GET("/logout", Logout)
     router.GET("/me", Me)
+    router.POST("/me-submit", ProcessMe)
+    router.GET("/images", Images)
+    router.POST("/images-upload", ProcessUpload)
     router.ServeFiles("/assets/*filepath", http.Dir("assets/"))
-    
+    router.ServeFiles("/storage/*filepath", http.Dir("storage/"))
+
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    if !isLoggedIn(r) {
-        http.Redirect(w, r, "/login", http.StatusSeeOther)
+    u, err := getUser(w, r)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        Logout(w, r, nil)
         return
     }
-    
-    tpl.ExecuteTemplate(w, "index.gohtml", nil)
+
+    var data Data
+
+    data.User = u
+
+    // Get the images for the user
+    img_dir := "/storage/uploads/" + strconv.Itoa(int(u.ID)) + "/"
+    files, _ := ioutil.ReadDir("." + img_dir)
+    // Fill the data.Images with the paths to images
+    for _, f := range files {
+        data.Images = append(data.Images, img_dir+f.Name())
+    }
+
+    tpl.ExecuteTemplate(w, "index.gohtml", data)
 }
 
 func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    // If already logged in just redirect to home
+    if isLoggedIn(r) {
+        http.Redirect(w, r, "/", http.StatusSeeOther)
+        return
+    }
+
     old_data := make(map[string]string, 2)
     // Get old data from cookie
     cookie, err := r.Cookie("old_data")
@@ -67,7 +107,7 @@ func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
             old_data[k] = strings.Join(v, "")
         }
     }
-    
+
     tpl.ExecuteTemplate(w, "login.gohtml", old_data)
 }
 
@@ -77,39 +117,41 @@ func ProcessLogin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
         http.Redirect(w, r, "/", http.StatusSeeOther)
         return
     }
-    
+
     if r.Method == http.MethodPost {
         // Get email and password from form
         email := r.FormValue("email")
         password := r.FormValue("password")
-        
+
         // Check if the required fields are set
         if len(email) == 0 || len(password) == 0 {
             http.Redirect(w, r, "/login", http.StatusSeeOther)
         }
-        
-        o := orm.NewOrm()
-        
-        u := &User{
-            Email:    email,
-            Password: password,
-        }
-        
-        // Get user from database
-        found := o.Read(u, "Email", "Password")
-        if found != nil {
+
+        u := User{}
+
+        q := db.First(&u, "email=?", email)
+        if q.Error != nil {
             expires := time.Now().Add(time.Second * 3)
             cookie := http.Cookie{Name: "old_data", Value: r.PostForm.Encode(), Expires: expires}
             http.SetCookie(w, &cookie)
-            
+
             http.Redirect(w, r, "/login", http.StatusSeeOther)
             return
         }
-        
+
+        // Check if passwords match
+        err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+        if err != nil {
+            fmt.Println(err)
+            http.Redirect(w, r, "/login", http.StatusSeeOther)
+            return
+        }
+
         // Set user session
         session := http.Cookie{Name: "session", Value: u.Email}
         http.SetCookie(w, &session)
-        
+
         http.Redirect(w, r, "/", http.StatusSeeOther)
     }
 }
@@ -125,7 +167,7 @@ func ProcessRegister(w http.ResponseWriter, r *http.Request, _ httprouter.Params
     email := r.FormValue("email")
     password := r.FormValue("password")
     confirm_password := r.FormValue("confirm_password")
-    
+
     // Check for required and if the password is confirmed
     if len(first_name) == 0 || len(last_name) == 0 || len(email) == 0 || len(password) == 0 || len(confirm_password) == 0 {
         http.Redirect(w, r, "/register", http.StatusSeeOther)
@@ -134,22 +176,33 @@ func ProcessRegister(w http.ResponseWriter, r *http.Request, _ httprouter.Params
         http.Redirect(w, r, "/register", http.StatusSeeOther)
         return
     }
-    
+
+    // Hashing the password
+    pass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+    check(err, w)
+
+    // Building the user model
     u := &User{
-        FirstName: first_name,
-        LastName:  last_name,
+        FirstName: strings.Title(first_name),
+        LastName:  strings.Title(last_name),
         Email:     email,
-        Password:  password,
+        Password:  string(pass),
     }
-    
-    o := orm.NewOrm()
-    
-    _, err := o.Insert(u)
-    if err != nil {
+
+    exists := db.First(&u, "email=?", email)
+    if exists.RowsAffected != 0 {
+        http.Error(w, "Email is taken", http.StatusUnauthorized)
+        http.Redirect(w, r, "/register", http.StatusSeeOther)
+        return
+    }
+
+    // Inserting the user into the DB
+    q := db.Save(u)
+    if q.Error != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
-    
+
     http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
@@ -161,12 +214,126 @@ func Logout(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
         MaxAge: -1,
     }
     http.SetCookie(w, c)
-    
+
     http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
 func Me(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    tpl.ExecuteTemplate(w, "me.gohtml", nil)
+    u, err := getUser(w, r)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        Logout(w, r, nil)
+        return
+    }
+
+    tpl.ExecuteTemplate(w, "me.gohtml", u)
+}
+
+func ProcessMe(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    r.ParseForm()
+    form := r.PostForm
+
+    // Check for empty fields
+    for field := range form {
+        value := form.Get(field)
+
+        if len(value) == 0 {
+            http.Error(w, field+" is required", http.StatusInternalServerError)
+            return
+        }
+    }
+
+    // Get User from database
+    u, err := getUser(w, r)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    // Check if Old password is correct
+    err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(form.Get("old_password")))
+    if err != nil {
+        http.Error(w, "The password is incorrect!", http.StatusInternalServerError)
+        return
+    }
+
+    // Check if the password is different
+    err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(form.Get("new_password")))
+    if err == nil {
+        http.Error(w, "New password is the same as the old one!", http.StatusInternalServerError)
+        return
+    }
+
+    // Check if the email is taken by somebody else
+    id := u.ID
+    q := db.Find(&User{}, "id != ? AND email = ?", id, form.Get("email"))
+    if q.RowsAffected > 0 {
+        http.Error(w, "Email is taken!", http.StatusInternalServerError)
+        return
+    }
+
+    password, err := bcrypt.GenerateFromPassword([]byte(form.Get("new_password")), bcrypt.DefaultCost)
+    check(err, w)
+
+    // Update the user
+    u.FirstName = strings.Title(form.Get("first_name"))
+    u.LastName = strings.Title(form.Get("last_name"))
+    u.Email = form.Get("email")
+    u.Password = string(password)
+
+    db.Save(&u)
+
+    // Update the session cookie
+    cookie := http.Cookie{Name: "session", Value: u.Email}
+    http.SetCookie(w, &cookie)
+
+    http.Redirect(w, r, "/me", http.StatusSeeOther)
+}
+
+func Images(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    if !isLoggedIn(r) {
+        http.Redirect(w, r, "/login", http.StatusSeeOther)
+        return
+    }
+
+    tpl.ExecuteTemplate(w, "images.gohtml", nil)
+}
+
+func ProcessUpload(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    u, err := getUser(w, r)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        Logout(w, r, nil)
+        return
+    }
+
+    mf, fh, err := r.FormFile("image")
+    check(err, w)
+    defer mf.Close()
+
+    ext := strings.Split(fh.Filename, ".")[1]
+    h := sha1.New()
+    io.Copy(h, mf)
+    fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+    // create new file
+    wd, err := os.Getwd()
+    if err != nil {
+        fmt.Println(err)
+    }
+    // Create the dir if it does not exist
+    dirpath := filepath.Join(wd, "storage", "uploads", strconv.Itoa(int(u.ID)))
+    os.MkdirAll(dirpath, 0777)
+    path := filepath.Join(dirpath, fname)
+    nf, err := os.Create(path)
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer nf.Close()
+    // copy
+    mf.Seek(0, 0)
+    io.Copy(nf, mf)
+
+    http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 // Check for errors and if they exist handle them with http
@@ -182,4 +349,32 @@ func isLoggedIn(r *http.Request) bool {
         return false
     }
     return true
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) (User, error) {
+    if !isLoggedIn(r) {
+        http.Redirect(w, r, "/login", http.StatusSeeOther)
+        return User{}, nil
+    }
+
+    c, err := r.Cookie("session")
+    check(err, w)
+
+    u := User{}
+
+    q := db.Find(&u, "email=?", c.Value)
+    if q.Error != nil {
+        expires := time.Now().Add(time.Second * 3)
+        cookie := http.Cookie{Name: "old_data", Value: r.PostForm.Encode(), Expires: expires}
+        http.SetCookie(w, &cookie)
+
+        http.Redirect(w, r, "/login", http.StatusSeeOther)
+        return User{}, errors.New("User not found")
+    }
+
+    return u, nil
+}
+
+func (u User) FullName() string {
+    return strings.Title(u.FirstName + " " + u.LastName)
 }
