3074c6f96f2299af37246e0578624b5e6818e0c0
mihailomisic <mihailo.misic@gmail.com>
2017-06-20 19:26:17 +0200

    Benchmarking nilz

--- 11-nil-value-benchmark/nilz_map.go
+++ 11-nil-value-benchmark/nilz_map.go
@@ -14,27 +14,6 @@ func Byte() {
 	}
 }
 
-func Byte1() {
-	m := make(map[int]byte, 100000)
-	for i := 0; i < 100000; i++ {
-		m[i] = 0
-	}
-}
-
-func Uint() {
-	m := make(map[int]uint8, 100000)
-	for i := 0; i < 100000; i++ {
-		m[i] = 0
-	}
-}
-
-func Uint1() {
-	m := make(map[int]uint8, 100000)
-	for i := 0; i < 100000; i++ {
-		m[i] = 1
-	}
-}
-
 func Int() {
 	m := make(map[int]int8, 100000)
 	for i := 0; i < 100000; i++ {
@@ -42,37 +21,9 @@ func Int() {
 	}
 }
 
-func Int1() {
-	m := make(map[int]int8, 100000)
-	for i := 0; i < 100000; i++ {
-		m[i] = 0
-	}
-}
-
-func String() {
-	m := make(map[int]string, 100000)
-	for i := 0; i < 100000; i++ {
-		m[i] = ""
-	}
-}
-
-func String1() {
-	m := make(map[int]string, 100000)
-	for i := 0; i < 100000; i++ {
-		m[i] = "1"
-	}
-}
-
-func BoolF() {
+func Bool() {
 	m := make(map[int]bool, 100000)
 	for i := 0; i < 100000; i++ {
 		m[i] = false
-	}
-}
-
-func BoolT() {
-	m := make(map[int]bool, 100000)
-	for i := 0; i < 100000; i++ {
-		m[i] = true
 	}
 }
--- 11-nil-value-benchmark/nilz_map_test.go
+++ 11-nil-value-benchmark/nilz_map_test.go
@@ -14,56 +14,14 @@ func Benchmark_Byte(b *testing.B) {
 	}
 }
 
-func Benchmark_Byte1(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		Byte1()
-	}
-}
-
-func Benchmark_Uint(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		Uint()
-	}
-}
-
-func Benchmark_Uint1(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		Uint1()
-	}
-}
-
 func Benchmark_Int(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		Int()
 	}
 }
 
-func Benchmark_Int1(b *testing.B) {
+func Benchmark_Bool(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		Int1()
-	}
-}
-
-func Benchmark_String(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		String()
-	}
-}
-
-func Benchmark_String1(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		String1()
-	}
-}
-
-func Benchmark_BoolF(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		BoolF()
-	}
-}
-
-func Benchmark_BoolT(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		BoolT()
+		Bool()
 	}
 }
