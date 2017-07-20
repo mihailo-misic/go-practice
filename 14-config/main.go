a323ccc743a6ee90e3363ed9d7424fc74b559dd3
mihailo <mihailo.misic@gmail.com>
2017-07-20 22:20:16 +0200

    NEW conf practice

--- 14-config/main.go
+++ 14-config/main.go
@@ -11,11 +11,11 @@ func main() {
 	viper.SetConfigName("config")
 	viper.AddConfigPath("./conf")
 	viper.AddConfigPath(".")
-	
+
 	err := viper.ReadInConfig() // Find and read the config file
-	if err != nil { // Handle errors reading the config file
+	if err != nil {             // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
-	
+
 	fmt.Println(viper.Get("owner.name"))
 }
