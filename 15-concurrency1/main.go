d376b13db8824eb93240c4235e8547018336e67e
mihailo <mihailo.misic@gmail.com>
2018-03-27 21:19:12 +0200

    UPDATE: concurrency2 finished

--- 15-concurrency1/main.go
+++ 15-concurrency1/main.go
@@ -17,35 +17,35 @@ var Alice = Person{Name: "Alice"}
 
 func main() {
 	ch := make(chan string)
-
+	
 	fmt.Println("Let's go for a walk!")
-
+	
 	// Getting ready.
 	go Bob.GetReady(ch)
 	go Alice.GetReady(ch)
-
+	
 	for i := 0; i < 2; i++ {
 		<-ch
 	}
-
+	
 	// Arming alarm and putting shoes.
 	go Alarm(ch)
 	time.Sleep(1 * time.Second)
 	go Bob.PutShoes(ch)
 	go Alice.PutShoes(ch)
-
+	
 	for i := 0; i < 2; i++ {
 		<-ch
 	}
-
+	
 	fmt.Println("Exiting and locking the door.")
-
+	
 	<-ch
 }
 
 func (p *Person) GetReady(ch chan string) {
 	fmt.Println(p.Name + " started getting ready...")
-	d := time.Duration(RandRange(10, 25)) * time.Second
+	d := time.Duration(RandRange(5, 10)) * time.Second
 	time.Sleep(d)
 	fmt.Println(p.Name + " spent " + d.String() + " getting ready.")
 	ch <- "done"
@@ -55,20 +55,25 @@ func Alarm(ch chan string) {
 	fmt.Println("Arming alarm.")
 	time.Sleep(5 * time.Second)
 	fmt.Println("Alarm is counting down...")
-	time.Sleep(20 * time.Second)
+	time.Sleep(10 * time.Second)
 	fmt.Println("Alarm is armed!")
 	ch <- "done"
 }
 
 func (p *Person) PutShoes(ch chan string) {
 	fmt.Println(p.Name + " started putting on shoes...")
-	d := time.Duration(RandRange(10, 20)) * time.Second
+	d := time.Duration(RandRange(5, 10)) * time.Second
 	time.Sleep(d)
 	fmt.Println(p.Name + " spent " + d.String() + " putting on shoes.")
 	ch <- "done"
 }
 
 // Helpers
+
+// Seeding the math/rand
+var seed = rand.NewSource(time.Now().UnixNano())
+var r = rand.New(seed)
+
 func RandRange(min, max int) int {
-	return rand.Intn(max-min) + min
+	return r.Intn(max-min) + min
 }
--- 16-concurrency2/main.go
+++ 16-concurrency2/main.go
@@ -17,47 +17,53 @@ Usage time 15m - 2h
 
 type Tourist struct {
 	Name string
-	Done bool
 }
 
 var Tourists []Tourist
-
-var seed = rand.NewSource(time.Now().UnixNano())
-var r = rand.New(seed)
-
-var bc = 0
 
 func init() {
 	genTourists()
 }
 
 func main() {
-	busy := make(chan int, len(Tourists))
-	busy <- 0
-
+	done := make(chan int, len(Tourists))
+	bc := 0
+	
 	for _, t := range Tourists {
+		ct := t
 		if bc < 8 {
-			go t.UsePC(busy)
-			<-busy
+			go ct.UsePC(done)
+		} else {
+			<-done
+			go ct.UsePC(done)
 		}
+		bc++
+	}
+	
+	// Wait for the last 8 to finish.
+	for i := 0; i < 8; i++ {
+		<-done
 	}
 }
 
 func (t *Tourist) UsePC(c chan int) {
 	fmt.Printf("Tourist %s is online.\n", t.Name)
-	bc++
 	d := time.Duration(randRange(5, 10)) * time.Second
 	time.Sleep(d)
 	fmt.Printf("Tourist %s is done - spent %s online.\n", t.Name, d)
-	bc--
+	c <- 1
 }
 
 // Helpers
 func genTourists() {
 	for i := 1; i <= 25; i++ {
-		Tourists = append(Tourists, Tourist{strconv.Itoa(i), false})
+		Tourists = append(Tourists, Tourist{strconv.Itoa(i)})
 	}
 }
+
+// Seeding the math/rand
+var seed = rand.NewSource(time.Now().UnixNano())
+var r = rand.New(seed)
 
 func randRange(min, max int) int {
 	return r.Intn(max-min) + min
